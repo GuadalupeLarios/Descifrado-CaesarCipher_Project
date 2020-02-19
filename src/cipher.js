de930d17a8fb275d8691a3edce399ad681f083c7
GuadalupeLarios <gdlp.larios@gmail.com>
2020-02-19 16:14:29 -0500

    primer commit

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,27 @@
 const cipher = {
-  // ...
+
+encode: (offset,valorInput) => {
+
+      let palabraCifrada ='';
+
+      for (let i=0; i<valorInput.length; i++){
+      let codigo = (valorInput.charCodeAt(i)-65+parseInt(offset))%26+65;
+      palabraCifrada= palabraCifrada + String.fromCharCode(codigo);
+
+}
+return palabraCifrada
+},
+
+decode: (offset1,valorInput1) =>{
+
+       let descifrado ='';
+
+       for (let i=0; i<valorInput1.length; i++){
+       let codigo = (valorInput1.charCodeAt(i)+65-parseInt(offset1))%26+65;
+       descifrado= descifrado + String.fromCharCode(codigo);
+}
+return descifrado
+}
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const boton = document.getElementById('btn');
+
+boton.addEventListener ('click',()=>{
+
+        let valorInput = document.getElementById('respuesta').value.toUpperCase();
+        let offset = document.getElementById('offsetresultado').value;
+        let palabraCifrada=cipher.encode(offset,valorInput);
+        document.getElementById('cifrado').value=palabraCifrada;
+})
+
+
+const botonDescifrado = document.getElementById('btnfinal');
+    botonDescifrado.addEventListener ('click',()=>{
+
+        let valorInput1 = document.getElementById('final').value.toUpperCase();
+        let offset1 = document.getElementById('offsetfinal').value;
+        let descifrado = cipher.decode(offset1,valorInput1);
+        document.getElementById('cifradofinal').value=descifrado;
+});
