587e9654d180aec5399f1f46eea2f94e958d0c8d
GuadalupeLarios <gdlp.larios@gmail.com>
2020-03-03 11:04:22 -0500

    0303

--- src/cipher.js
+++ src/cipher.js
@@ -1,12 +1,17 @@
 const cipher = {
 
-encode: (offset,valorInput) => {
+encode: (offset,valorInput) =>{
 
       let palabraCifrada ='';
 
       for (let i=0; i<valorInput.length; i++){
-      let codigo = (valorInput.charCodeAt(i)-65+parseInt(offset))%26+65;
-      palabraCifrada= palabraCifrada + String.fromCharCode(codigo);
+
+        if (valorInput.charCodeAt(i) === 32) {
+           palabraCifrada += ' ';
+        } else {
+        let codigo = (valorInput.charCodeAt(i)-65+parseInt(offset))%26+65;
+        palabraCifrada= palabraCifrada + String.fromCharCode(codigo)
+        }
 
 }
 return palabraCifrada
@@ -14,14 +19,21 @@ return palabraCifrada
 
 decode: (offset1,valorInput1) =>{
 
-       let descifrado ='';
+      let descifrado ='';
 
-       for (let i=0; i<valorInput1.length; i++){
+      for (let i=0; i<valorInput1.length; i++){
+
+        if (valorInput1.charCodeAt(i) === 32) {
+           descifrado += ' ';
+       } else {
        let codigo = (valorInput1.charCodeAt(i)+65-parseInt(offset1))%26+65;
        descifrado= descifrado + String.fromCharCode(codigo);
+       }
+
 }
 return descifrado
 }
 };
+
 
 export default cipher;
